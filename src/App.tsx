09de38ade79fcf9a60352c53a1bdff4fd07c3b88
Wesly Rismahadi <122729640+theo00000@users.noreply.github.com>
2026-09-16 16:08:28 +0700

    feat: initialize project with React, Vite, and TypeScript setup

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import Quiz from "./pages/Quiz";
+import Result from "./pages/Result";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/quiz" element={<Quiz />} />
+        <Route path="/result" element={<Result />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
